521596ffb2a2d2a966594f4508de037cbd795b43
chocolatito <usuariodetelegram@gmail.com>
2022-01-28 20:59:56 -0300

    Setup JWT gem

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module MarketPlaceApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -33,5 +33,6 @@ module MarketPlaceApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
